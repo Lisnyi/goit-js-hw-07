729b4d2f94150f3a7c96bf15c795f6c1be22595b
Lisnyi <99568987+Lisnyi@users.noreply.github.com>
2022-07-18 17:07:42 +0300

    task 1, part 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery")
+
+const galleryMarkup = galleryItems.map(item => `<div class="gallery__item">
+<a class="gallery__link" href="${item.original}">
+  <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+  />
+</a>
+</div>`).join("")
+
+galleryRef.innerHTML = galleryMarkup
+
+function onGalleryCardClick (e) {
+    if (e.target.nodeName !== "IMG"){
+        return
+    }
+    e.preventDefault()
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">`)
+    instance.show()
+    instance.close((e)=>console.log(e))
+}
+
+galleryRef.addEventListener("click", onGalleryCardClick)
+
