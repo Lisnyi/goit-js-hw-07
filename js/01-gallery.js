4bf4d762da5c6192db65680e3674656069a8b500
Lisnyi <russelhobbbs@gmail.com>
2022-07-18 19:50:25 +0300

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,29 +3,39 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryRef = document.querySelector(".gallery")
 
-const galleryMarkup = galleryItems.map(item => `<div class="gallery__item">
-<a class="gallery__link" href="${item.original}">
-  <img
-    class="gallery__image"
-    src="${item.preview}"
-    data-source="${item.original}"
-    alt="${item.description}"
-  />
-</a>
-</div>`).join("")
+const galleryMarkup = galleryItems.map(item =>
+  `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+  </div>`).join("")
 
 galleryRef.innerHTML = galleryMarkup
 
-function onGalleryCardClick (e) {
-    if (e.target.nodeName !== "IMG"){
-        return
-    }
-    e.preventDefault()
-    const instance = basicLightbox.create(`
-    <img src="${e.target.dataset.source}" width="800" height="600">`)
-    instance.show()
-    instance.close((e)=>console.log(e))
-}
-
 galleryRef.addEventListener("click", onGalleryCardClick)
 
+function onGalleryCardClick (e) {
+  if (e.target.nodeName !== "IMG"){
+    return
+  }
+  e.preventDefault()
+  const instance = basicLightbox.create(`<img src="${e.target.dataset.source}" width="800" height="600">`)
+  instance.show()
+    
+  window.addEventListener("keydown", closeModalByEscape) 
+  
+  function closeModalByEscape(e) {
+    const keyName = e.key
+    if (keyName !== "Escape") {
+      return
+    }
+    instance.close()
+    
+    window.removeEventListener("keydown", closeModalByEscape)
+  }
+}
